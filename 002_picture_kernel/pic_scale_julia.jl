c7509d149e472b46af26dc8e999557237469ec55
tgautam03 <tushariitr3@gmail.com>
2022-01-18 14:31:40 -0700

    Corrected mistake

--- 002_picture_kernel/pic_scale_julia.jl
+++ 002_picture_kernel/pic_scale_julia.jl
@@ -1,19 +1,19 @@
 using CUDA
 
 # Defining Matrix dimensions
-row = 5
-col = 6
+row = 9
+col = 8
 
 # Declaring Matrix
-pic = Array{Float32}(rand(5,6))
+pic = Array{Float32}(rand(row,col))
 
 function scalePic(pic::Matrix{Float32}, row::Int, col::Int)
     # Allocate and transfer matrix to GPU Memory
     d_pic = CuArray(pic)
 
     # Kernel Invocation
-    dimBlock = (16, 16) # Threads in a block
-    dimGrid = (Int(ceil(row/dimBlock[1])), Int(ceil(row/dimBlock[2]))) # Blocks in a grid
+    dimBlock = (2, 2) # Threads in a block
+    dimGrid = (Int(ceil(col/dimBlock[1])), Int(ceil(row/dimBlock[2]))) # Blocks in a grid
 
     @cuda threads=dimBlock blocks=dimGrid scalePicKernel!(d_pic, row, col)
     
